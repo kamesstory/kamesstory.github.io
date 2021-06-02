8820b63059fb94cf64805e669b98eeeb7a6f3157
Jason Wang <jhw513@gmail.com>
2021-06-01 18:54:47 -0700

    better links

--- main.js
+++ main.js
@@ -4,5 +4,16 @@
 
 const onCohereLinkClick = () => window.open("https://cohere.so");
 
+const onFacebookLinkClick = () =>
+  window.open(
+    "https://about.fb.com/news/2018/06/all-of-your-facebook-memories-are-now-in-one-place/"
+  );
+const onSiemensLinkClick = () =>
+  window.open("https://www.plm.automation.siemens.com/");
+const onFBNLinkClick = () =>
+  window.open(
+    "https://www.bloomberg.com/news/articles/2020-08-03/farmers-business-network-raises-funds-at-1-75-billion-valuation"
+  );
+
 const onLinkedinLinkClick = () =>
   window.open("https://linkedin.com/in/jason-hf-wang");
