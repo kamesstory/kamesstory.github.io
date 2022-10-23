aea191773010fd8be30a35abb0e9bd85dce4a8c4
Jason Wang <jhw513@gmail.com>
2022-10-23 14:46:44 -0700

    cleanup

--- main.js
+++ main.js
@@ -23,17 +23,3 @@ const onLinkedinLinkClick = () =>
 const onGithubLinkClick = () => window.open("https://github.com/kamesstory");
 
 const onEmailLinkClick = () => window.open("mailto:jhw513@gmail.com");
-
-const linkElements = document.querySelectorAll(".link");
-const overlayElement = document.querySelector(".overlay-blur");
-
-linkElements.forEach((e) => {
-  e.addEventListener("mouseenter", () => {
-    e.classList.add("focused-above-overlay");
-    // console.log(`adding focused above overlay`);
-  });
-  e.addEventListener("mouseleave", () => {
-    e.classList.remove("focused-above-overlay");
-    // console.log(`removing focused above overlay`);
-  });
-});
