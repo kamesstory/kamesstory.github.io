841bbad26cc35fd294264d09174074231885133a
Jason Wang <jhw513@gmail.com>
2023-01-18 23:17:07 -0800

    no more link underlined anywhere

--- stream/main.js
+++ stream/main.js
@@ -6,12 +6,6 @@ fetch("https://jasonwa.ng/stream/thoughts.md")
   .then((response) => response.text())
   .then((text) => {
     const parsed = marked.parse(text);
-    const parsedWithClasses = parsed.replaceAll(
-      `<a`,
-      `<a class="link underlined"`
-    );
-
     const lookingGlassContainer = document.querySelector(".stream-container");
-
-    lookingGlassContainer.innerHTML = parsedWithClasses;
+    lookingGlassContainer.innerHTML = parsed;
   });
